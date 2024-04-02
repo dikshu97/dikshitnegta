bb240f8a30f451aa3751beec589ed602e3dc7381
dikshit <dikshitnegta32@gmail.com>
2024-04-03 00:09:25 +0530

    commite js

---
+++ Class/class.js
@@ -0,0 +1,84 @@
+// let i=1;
+// while(i<=5) {
+//     for(let j=1;j<=10;j++){
+//         console.log("dikshu",i ,j);
+//     }
+//     i++;
+// }
+// console.log(("hiii"));
+// console.log("dikshu");
+// console.log(23);
+// console.log(true);
+// console.log(false);
+// console.log(null);
+// console.log(undefined);
+// process.stdout.write("hello");
+// process.stdout.write("78998");
+
+// let name="dikshu";
+// let profession="student";
+// let age=25;
+// let married=false;
+// let salary=9098;
+// let appraisal="null";
+// console.log(name);
+// console.log(profession);
+// console.log(age);
+// console.log(married);
+// console.log(salary);
+// console.log(appraisal);
+// process.stdout.write("dikshu"+"\n")
+// process.stdout.write("negta"+"\t"+"student");
+
+//
+// let age=18;
+// if (age<18){
+//     console.log("go to school");
+// }
+// if (age==18){
+//     console.log("go to hell");
+// }
+// if (age>18){
+//     console.log("dtill go to hell");
+// }
+// let age=18
+// if (age<16){
+//     console.log("go to school");
+// } else {
+//     console.log("go to college");
+// }
+// let age=20;
+// if (age<18){
+//     console.log("go to school");
+// }else{
+//     if (age==18){
+//         console.log("you are an adult");
+//     }else{
+//         if (age==20){
+//             console.log("go to date");
+//         } else {
+//             if (age<25){
+//                 console.log("get a job");
+//             } else {
+//                 if (age==30){
+//                     console.log("get married");
+//                 }
+//             }
+//         }
+//     }
+// }
+let age=20
+if (age<20){
+    console.log("go to school");
+} else if (age==18)  {
+console.log("you are an adult");
+} else if (age>20){
+    console.log("go to college");
+} else if (age<25) {
+    console.log("go to hell");
+}
+else if (age>30) {
+console.log("get married");
+}
+
+
